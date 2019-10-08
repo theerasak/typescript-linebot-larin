917aa54c5e1c9eba07bdf6f94249d8d7e3fbe91e
Theerasak Maneeneim <theerasak.maneeneim@gmail.com>
2019-10-08 16:06:44 +0700

    init linebot

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var world = 'Theerasak-w';
+function hello(word) {
+    if (word === void 0) { word = world; }
+    return "Hello " + world + "! ";
+}
+exports.hello = hello;
---
+++ index.ts
@@ -0,0 +1,5 @@
+const world = 'Theerasak-w';
+
+export function hello(word: string = world): string {
+  return `Hello ${world}! `;
+}
